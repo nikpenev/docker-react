5c3d5860e27e0af36b7148e91152b8f96d52b5ea
root <root@localhost.localdomain>
2019-07-04 08:21:05 +0300

    travis update

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-        Shit`s truly awesome!!!
+	This is a test Docker project.
 	</p>
         <a
           className="App-link"
