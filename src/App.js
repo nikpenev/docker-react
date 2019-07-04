fab6a38135ed7d040365e3b94afeb9403bc69a82
root <root@localhost.localdomain>
2019-07-04 09:09:20 +0300

    branch change

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-	This is a test Docker project.
+	This is a test Docker project. This is the branch change.
 	</p>
         <a
           className="App-link"
